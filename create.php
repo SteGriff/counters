69be62ba083128fe571395b8510f00bacd497cf0
SteGriff <github@stegriff.co.uk>
2016-11-23 10:12:30 +0000

    Add comments to create.php

--- create.php
+++ create.php
@@ -3,6 +3,7 @@
 
 require 'DAL.php';
 
+//Get name (required)
 $name = $_GET['Name'];
 if ($name == null)
 {
@@ -10,27 +11,41 @@ if ($name == null)
 	die();
 }
 
+//Get description (required)
 $description = $_GET['Description'];
 if ($description == null)
 {
 	echo "No Description!";
 	die();
 }
-	
-$font = !empty($_GET['Font'])
-	? $_GET['Font'] : 'fonts/ARIAL.TTF';
-$fontSize = !empty($_GET['FontSize'])
-	? $_GET['FontSize'] : 16;
-	
-$r = !empty($_GET['R'])
-	? $_GET['R'] : 10;
-$g = !empty($_GET['G'])
-	? $_GET['G'] : 10;
-$b = !empty($_GET['B'])
-	? $_GET['B'] : 10;
 
+//Get font or default to Arial
+$font = !empty($_GET['Font'])
+	? $_GET['Font']
+	: 'fonts/ARIAL.TTF';
+	
+//Get font size or default to 16
+$fontSize = !empty($_GET['FontSize'])
+	? $_GET['FontSize']
+	: 16;
+	
+//Get colour in RGB or default to (10,10,10) (that's dark grey)
+$r = !empty($_GET['R'])
+	? $_GET['R']
+	: 10;
+	
+$g = !empty($_GET['G'])
+	? $_GET['G']
+	: 10;
+	
+$b = !empty($_GET['B'])
+	? $_GET['B']
+	: 10;
+
+//Create counter in the database and get success/fail
 $result = createCounter($name, $description, $font, $fontSize, $r, $g, $b);
 
+//Return success message
 if ($result !== false)
 {
 	echo "Created counter $result!";
